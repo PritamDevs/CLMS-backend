93eede03681850aecee670a0424d01cc4459aafc
PritamDevs <debpritam2004@gmail.com>
2025-09-18 20:28:48 +0530

    v1.1.8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let app=express()
 const connectDB = require('./config/db.config')
 dotenv.config()
 const cors =require('cors')
-const path=require('path')
+
 
 //middleware
 app.use(express.urlencoded({extended:true}))
@@ -16,12 +16,7 @@ app.use(cors({
     credentials:true
 }))
 
-app.use((err, req, res, next) => {
-  if (err instanceof cors.CorsError) {
-    return res.status(403).json({ message: 'CORS error', success: false });
-  }
-  next(err);
-});
+
 
 let studentRoute = require('./routes/student.route')
 app.use('/api/student',studentRoute)
@@ -31,7 +26,7 @@ let bookRoute = require('./routes/book.route')
 app.use('/api/book',bookRoute)
 let issueRequestRoute = require('./routes/issueRequest.route');
 app.use('/api/request', issueRequestRoute)
-// app.use('/api/student', require('./routes/student.route'));
+app.use('/api/student', require('./routes/student.route'));
 
 if (process.env.NODE_ENV === "production") {
     const frontendPath = path.join(__dirname, "dist"); 
@@ -48,6 +43,3 @@ connectDB()
         console.log(`Server is running on port ${process.env.PORT}`)
     })
 })
-.catch(err => {
-    console.error("DB connection failed:", err);
-});
